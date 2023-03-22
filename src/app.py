ccdf77ff58e34530b782a74d5118735d64ac33b7
Ken Wang <kenuiuc@gmail.com>
2023-03-22 16:10:24 -0700

    add density plot

--- src/app.py
+++ src/app.py
@@ -21,21 +21,48 @@ def plot_bar(xmax):
     )
     return chart.to_html()
 
+
+def plot_density(xmax):
+    density = alt.Chart(
+        data[data['current_land_value'] < xmax],
+        title='Price by Area'
+    ).transform_density(
+        'current_land_value',
+        groupby=['Geo Local Area'],
+        as_=['current_land_value', 'density'],
+    ).mark_area(
+        opacity=0.6
+    ).encode(
+        x=alt.X('current_land_value',title='Price'),
+        y='density:Q',
+        color='Geo Local Area'
+    )
+    return density.to_html()
+
+
 app.layout = html.Div([
+        html.H1('Vancouver Housing Price'),
+        dcc.Slider(id='xslider', min=0, max=5000000, value=2500000),
+        html.Iframe(
+            id='density',
+            srcDoc=plot_density(xmax=2500000),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+        ),
         html.Iframe(
             id='bar',
             srcDoc=plot_bar(xmax=2500000),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Slider(id='xslider', min=0, max=5000000, value=2500000,)
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}
+        )
 ])
 
 @app.callback(
     Output('bar', 'srcDoc'),
+    Output('density', 'srcDoc'),
     Input('xslider', 'value')
 )
 
 def update_output(xmax):
-    return plot_bar(xmax)
+    return plot_bar(xmax), plot_density(xmax)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
