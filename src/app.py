f9ad19a2deaa9121a273f9c746798a89af96b1ce
Ken Wang <kenuiuc@gmail.com>
2023-03-22 15:33:24 -0700

    simple bar chart with slide bar

--- src/app.py
+++ src/app.py
@@ -1,44 +1,41 @@
-from dash import dash, html
+import pandas as pd
+from dash import dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
+import altair as alt
 
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Hi Ken'),
-    html.Iframe(
-        srcDoc=plot_density(),
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}
-    ),
-    html.Iframe(
-        srcDoc=plot_bar(),
-        style={'border-width': '0', 'width': '100%', 'height': '400px'}
-    )
-])
+data = pd.read_csv('data/van_house.csv')
 
-def plot_bar():
-    return alt.Chart(data).mark_bar().encode(
+def plot_bar(xmax):
+    chart = alt.Chart(
+        data[data['current_land_value'] < xmax]
+    ).mark_bar().encode(
         x=alt.X("current_land_value", title="Price"),
         y=alt.Y("zoning_classification", title="Zoning Type"),
         color='zoning_classification',
         tooltip='current_land_value'
     ).properties(
         title="Price by Zoning Type"
-    ).to_html()
+    )
+    return chart.to_html()
 
+app.layout = html.Div([
+        html.Iframe(
+            id='bar',
+            srcDoc=plot_bar(xmax=2500000),
+            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+        dcc.Slider(id='xslider', min=0, max=5000000, value=2500000,)
+])
 
-def plot_density():
-    return alt.Chart(data, title='Price by Area').transform_density(
-        'current_land_value',
-        groupby=['Geo Local Area'],
-        as_=['current_land_value', 'density'],
-    ).mark_area(
-        opacity=0.6
-    ).encode(
-        x=alt.X('current_land_value',title='Price'),
-        y='density:Q',
-        color='Geo Local Area'
-    ).to_html()
+@app.callback(
+    Output('bar', 'srcDoc'),
+    Input('xslider', 'value')
+)
 
+def update_output(xmax):
+    return plot_bar(xmax)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
